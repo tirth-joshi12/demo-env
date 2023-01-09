f1f92a6f574233d73bea2964e613c6b57af8a4ec
tirth-joshi12 <121799663+tirth-joshi12@users.noreply.github.com>
2023-01-09 17:31:13 +0530

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -2,8 +2,8 @@
 set -e
 echo "Deploying to ${DEPLOYMENT_ENVIRONMENT}"
 echo $ACCOUNT_KEY_STAGING > service_key.txt
-base64 -i service_key.txt -d > gcloud-service-key.json
-gcloud auth activate-service-account ${ACCOUNT_ID} --key-file gcloud-service-key.json
+base64 -i service_key.txt -d > ${HOME}/gcloud-service-key.json
+gcloud auth activate-service-account ${ACCOUNT_ID} --key-file ${HOME}/gcloud-service-key.json
 gcloud config set project $PROJECT_ID
 gcloud --quiet config set container/cluster $CLUSTER_NAME
 gcloud config set compute/zone $CLOUDSDK_COMPUTE_ZONE
